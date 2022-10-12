59f50fac1ab7f5d6ae388bda4f1043c27da03f55
Christian Anker Larsen <christian.anker.larsen@maersk.com>
2022-10-12 15:00:08 +0200

    refactoring

--- client/funcs.js
+++ client/funcs.js
@@ -1,4 +1,141 @@
+let intervalID;
+let minutes;
+let seconds;
+let breakMode;
+let totalTime;
+
 export function convertMinsToSecs (minutes){
     const secsInMinutes = 60
     return minutes * secsInMinutes;
 }
+
+export function initialiseApp() {
+    console.log("starting app");
+    let buttonText = document.getElementById("button").innerText;
+    if (buttonText === 'START') {
+        if (totalTime === 1500) {
+            playWorkSound();
+        }
+        document.getElementById("button").innerHTML = "STOP";
+        breakMode = false;
+        countDown(breakMode);
+    }
+    if (buttonText === "STOP") {
+        clearInterval(intervalID);
+        document.getElementById("button").innerHTML = "START";
+    }
+}
+
+export function setDefaultTimes(){
+    console.log("page loaded");
+    minutes = 25;
+    totalTime = minutes * 60;
+    document.getElementById('pomodoro').value = minutes;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = '00';
+}
+
+
+function playWorkSound(){
+    const work = new Audio('resources/work.mp3');
+    work.play();
+}
+
+function playBreakSound(){
+    const work = new Audio('resources/break.mp3');
+    work.play();
+}
+
+function startBreak(){
+    breakMode = true;
+    document.getElementById("minutes").innerHTML = "00";
+    totalTime = convertMinsToSecs(document.getElementById('shortbreak').value);
+    //totalTime = document.getElementById('shortbreak').value * 60;
+    playBreakSound();
+    countDown(breakMode);
+}
+
+function countDown(onABreak){
+    changeBgColor();
+    if (onABreak){
+        intervalID = setInterval(() => {
+            if (totalTime <= 0) {
+                clearInterval(intervalID);
+                //document.getElementById("button").innerHTML = "START";
+                startWork();
+            }
+            decrementTime();
+        }, 1000);
+    }
+    else {
+        intervalID = setInterval(() => {
+            if (totalTime <= 0) {
+                clearInterval(intervalID);
+                //document.getElementById("button").innerHTML = "START";
+                startBreak();
+            }
+            decrementTime();
+        }, 1000);
+    }
+}
+
+function startWork(){
+    breakMode = false;
+    playWorkSound();
+    if (document.getElementById("pomodoro").value != null){
+        minutes = document.getElementById("pomodoro").value;
+    }
+    else {
+        minutes = 25;
+    }
+    document.getElementById("minutes").innerHTML = minutes;
+    totalTime = minutes * 60;
+    countDown(breakMode);
+}
+
+function decrementTime(){
+    totalTime--;
+    minutes = Math.floor((totalTime % (1000 * 60 * 60)) / 60);
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+    seconds = totalTime % 60;
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}
+
+function changeBgColor(){
+    if (breakMode){
+        document.getElementById("circle").style.backgroundColor = "green"
+    }
+    else {
+        document.getElementById("circle").style.backgroundColor = "red"
+    }
+}
+
+function callVisitorLogger(){
+    const loggerURL = "https://timed-tomatoes-page-load-logger.azurewebsites.net/api/visitorlogger"
+    fetch(loggerURL).then(res => console.log(res) );
+}
+
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+
+export function openSettings() {
+    modal.style.display = "block";
+}
+
+export function closeSpan(){
+    modal.style.display = "none";
+}
+export function updateTimes(){
+    totalTime = document.getElementById("pomodoro").value * 60;
+    minutes = document.getElementById("pomodoro").value;
+    document.getElementById("minutes").innerHTML = minutes;
+    closeSpan();
+}
+
+
--- client/funcs.test.js
+++ client/funcs.test.js
@@ -3,3 +3,7 @@ import {convertMinsToSecs} from "./funcs";
 it('takes minutes and returns total seconds', () => {
     expect(convertMinsToSecs(25)).toBe(1500);
 });
+
+it('tests', () => {
+    expect(document).toBeTruthy();
+})
