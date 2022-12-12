72948387ed8e1b61f356010778c9d9e2d528e653
Christian Anker Larsen <christian.anker.larsen@maersk.com>
2022-12-12 21:35:55 +0100

    refactoring

--- client/funcs.test.js
+++ client/funcs.test.js
@@ -1,13 +1,10 @@
-import { convertMinsToSecs, PomodoroApp, appendZero } from "./funcs";
+import {appendZero, changeStateToBreak, convertMinsToSecs, PomodoroApp} from "./funcs";
+
 let app = new PomodoroApp(true);
 
 it('takes minutes and returns total seconds', () => {
     expect(convertMinsToSecs(25)).toBe(1500);
 });
-
-function changeStateToBreak() {
-    app.breakMode = true;
-}
 
 it('returns the state of the app', () => {
     changeStateToBreak();
