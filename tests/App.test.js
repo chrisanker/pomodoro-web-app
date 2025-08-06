06a74e18cc7017edfea36ff3d81807e99d5e9cf5
Christian Anker Larsen <christian.anker.larsen@lns.maersk.com>
2025-08-06 16:40:02 +0200

    move describe block to top level

--- tests/App.test.js
+++ tests/App.test.js
@@ -6,68 +6,67 @@ describe('App', () => {
     const data = App.data()
     expect(data.timeDisplay).toBe('25:00')
   })
+})
+describe('start button', () => {
+  let data;
+  
+  afterEach(() => {
+    data = null
+  })
 
-  describe('start button', () => {
-    let data;
-    
-    afterEach(() => {
-      data = null
-    })
+  it('has a start button that is initially enabled', () => {
+    data = App.data()
+    expect(data.isRunning).toBe(false)
+  })
 
-    it('has a start button that is initially enabled', () => {
-      data = App.data()
-      expect(data.isRunning).toBe(false)
-    })
+  it('sets isRunning to true when start button is clicked', () => {
+    const app = App.methods
+    data = { isRunning: false }
 
-    it('sets isRunning to true when start button is clicked', () => {
-      const app = App.methods
-      data = { isRunning: false }
+    app.startTimer.call(data)
+    expect(data.isRunning).toBe(true)
+  })
 
-      app.startTimer.call(data)
-      expect(data.isRunning).toBe(true)
-    })
+  it('starts counting down when timer starts', () => {
+    const app = App.methods
+    data = {
+      isRunning: false,
+      minutes: 25,
+      seconds: 0,
+      timeDisplay: '25:00',
+      updateDisplay: App.methods.updateDisplay
+    }
 
-    it('starts counting down when timer starts', () => {
-      const app = App.methods
-      data = {
-        isRunning: false,
-        minutes: 25,
-        seconds: 0,
-        timeDisplay: '25:00',
-        updateDisplay: App.methods.updateDisplay
-      }
+    jest.useFakeTimers()
+    app.startTimer.call(data)
+    jest.advanceTimersByTime(1000)
 
-      jest.useFakeTimers()
-      app.startTimer.call(data)
-      jest.advanceTimersByTime(1000)
+    expect(data.seconds).toBe(59)
+    expect(data.minutes).toBe(24)
+    jest.useRealTimers()
+  })
 
-      expect(data.seconds).toBe(59)
-      expect(data.minutes).toBe(24)
-      jest.useRealTimers()
-    })
+  it('updates timeDisplay for each second of countdown', () => {
+    const app = App.methods
+    data = {
+      isRunning: false,
+      minutes: 25,
+      seconds: 0,
+      timeDisplay: '25:00',
+      updateDisplay: App.methods.updateDisplay
+    }
 
-    it('updates timeDisplay for each second of countdown', () => {
-      const app = App.methods
-      data = {
-        isRunning: false,
-        minutes: 25,
-        seconds: 0,
-        timeDisplay: '25:00',
-        updateDisplay: App.methods.updateDisplay
-      }
+    jest.useFakeTimers()
+    app.startTimer.call(data)
 
-      jest.useFakeTimers()
-      app.startTimer.call(data)
+    // After 1 second
+    jest.advanceTimersByTime(1000)
+    expect(data.timeDisplay).toBe('24:59')
 
-      // After 1 second
-      jest.advanceTimersByTime(1000)
-      expect(data.timeDisplay).toBe('24:59')
+    // After another second
+    jest.advanceTimersByTime(1000)
+    expect(data.timeDisplay).toBe('24:58')
 
-      // After another second
-      jest.advanceTimersByTime(1000)
-      expect(data.timeDisplay).toBe('24:58')
-
-      jest.useRealTimers()
-    })
+    jest.useRealTimers()
   })
 })
